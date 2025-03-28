081545e037d2a24fd2eec30ca27b0e1adbeb9e25
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-03-28 08:29:17 -0300

    Modificação da função __str__ de Categoria.

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -4,3 +4,5 @@ from django.db import models
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
     
+    def __str__(self):
+        return f"(0{self.id}) {self.descricao}"
