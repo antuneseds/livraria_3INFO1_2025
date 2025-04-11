ed2879ded4d80a84111ee6cbc62cc2f82a084040
antuneseds <esterdossantosantunes04@gmail.com>
2025-04-11 09:21:14 -0300

    Crianção da API Livro

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, UserViewSet
 
 router = DefaultRouter()
 
