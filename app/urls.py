ce191ee79fb975647ed5f093cb3b61836d34b5c5
antuneseds <esterdossantosantunes04@gmail.com>
2025-04-22 15:41:56 -0300

    feat: inclusão do relacionamento de Livro com Categoria e Editora

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ router = DefaultRouter()
 router.register(r'autores', AutorViewSet, basename='autores')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
+router.register(r'livros', LivroViewSet, basename='livros')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
